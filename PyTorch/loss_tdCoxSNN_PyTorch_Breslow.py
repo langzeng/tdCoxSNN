ecea12aec2f4a5881741d4f14f5a2c805f0eb94f
Lang Zeng <zenglang95@gmail.com>
2024-06-13 15:26:04 -0400

    Update and rename loss_tdCoxSNN_PyTorch_Brewslow to loss_tdCoxSNN_PyTorch_Breslow.py

---
+++ PyTorch/loss_tdCoxSNN_PyTorch_Breslow.py
@@ -0,0 +1,46 @@
+import torch
+
+def loss_tdCoxSNN_PyTorch(y_pred, y_true):
+
+    y_true = y_true.type(torch.float32) # tstart, tstop, event
+    y_pred = y_pred.type(torch.float32)
+    y_pred = torch.flatten(y_pred)
+
+    n_sample = y_pred.shape[0]
+    
+    time0 = torch.flatten(y_true[:,0])
+    time = torch.flatten(y_true[:,1])
+    event = torch.flatten(y_true[:,2])
+
+    sort_index = torch.argsort(time)
+    time0 = time0[sort_index] # ascending order
+    time = time[sort_index]
+    event = event[sort_index]
+    y_pred = y_pred[sort_index]
+    
+    if torch.sum(event) == 0.: 
+        return torch.sum(event)
+    else:
+        time_event = time * event
+        eventtime,tie_count = torch.unique(torch.masked_select(time_event,torch.gt(time_event,0)),
+                                           return_counts=True)
+
+        at_risk_index = ((time0 < eventtime[:,None]) & (time >= eventtime[:,None])).type(torch.float32)
+        event_index = (time == eventtime[:,None]).type(torch.float32)
+        
+        # haz = exp(risk)
+        tie_haz = torch.mm(event_index, (torch.exp(torch.clip(y_pred,-20,20))*event).unsqueeze(1))
+        
+        tie_risk = torch.mm(event_index, (y_pred*event).unsqueeze(1))
+
+        cum_haz = torch.mm(at_risk_index, (torch.exp(torch.clip(y_pred,-20,20))).unsqueeze(1))
+
+        mask_tie_risk = torch.arange(torch.max(tie_count)) < (tie_count[:,None])
+        out0 = torch.zeros(mask_tie_haz.size(),dtype = torch.float32)
+
+        log_sum_haz = torch.where(mask_tie_risk,torch.ones(mask_tie_risk.size(),dtype = torch.float32),out0)*cum_haz
+        log_sum_haz = torch.where(mask_tie_risk,torch.log(log_sum_haz+1e-15),out0)
+        log_sum_haz = torch.sum(log_sum_haz)
+        log_lik = torch.sum(tie_risk)-log_sum_haz
+        
+        return torch.negative(log_lik)/n_sample
